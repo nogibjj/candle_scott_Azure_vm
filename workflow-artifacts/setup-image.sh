20d0973b42cd9c758835096ee0bd1c44139ceeec
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-11-02 18:30:44 +0000

    add GPU runner

--- workflow-artifacts/setup-image.sh
+++ workflow-artifacts/setup-image.sh
@@ -18,12 +18,29 @@ curl -O -L https://github.com/actions/runner/releases/download/v2.310.2/actions-
 # Extract the installer
 tar xzf $WORK_DIR/actions-runner-linux-x64-2.310.2.tar.gz
 
-# # Create a GitHub runner Token
-# TOKEN=$(curl -X POST \
-#             -H "Authorization: token $GITHUB_PAT" \
-#             -H "Accept: application/vnd.github.v3+json" \
-#             https://api.github.com/repos/nogibjj/candle_scott_Azure_vm/actions/runners/registration-token | grep token | cut -d '"' -f 4)
-# #
-# #
-# # Configure the runner
-# $WORK_DIR/config.sh --unattended --url https://github.com/nogibjj/candle_scott_Azure_vm --token $TOKEN
+
+# Install CUDA Toolkit and Drivers
+sudo apt-get update
+sudo apt-get install -y build-essential dkms
+sudo apt-get install -y linux-headers-$(uname -r)
+sudo apt-get install -y wget git
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.2-460.32.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.2-460.32.03-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-2-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+
+
+# install Rust CLI 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+. "$HOME/.cargo/env"
+
+export PATH=/usr/local/cuda-11.8/bin:$PATH
+export PATH=/home/ubuntu/.cargo/bin:$PATH
+
+# get candle-core 
+cargo new myapp
+cd myapp
+git clone https://github.com/huggingface/candle.git
