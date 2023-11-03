2bb0b04a7fc47182925e02f8447a2e47d9dfff80
Scott Lai <43226003+ScottLL@users.noreply.github.com>
2023-11-03 14:19:43 +0000

    add

--- workflow-artifacts/setup-image.sh
+++ workflow-artifacts/setup-image.sh
@@ -18,26 +18,26 @@ curl -O -L https://github.com/actions/runner/releases/download/v2.310.2/actions-
 # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.310.2.tar.gz
 
-# Install CUDA Toolkit and Drivers
-sudo apt-get update
-sudo apt-get install -y build-essential dkms
-sudo apt-get install -y linux-headers-$(uname -r)
-sudo apt-get install -y wget git
+# # Install CUDA Toolkit and Drivers
+# sudo apt-get update
+# sudo apt-get install -y build-essential dkms
+# sudo apt-get install -y linux-headers-$(uname -r)
+# sudo apt-get install -y wget git
 
-# Install CUDA Toolkit for Ubuntu 20.04
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda-drivers
+# # Install CUDA Toolkit for Ubuntu 20.04
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+# sudo dpkg -i cuda-keyring_1.0-1_all.deb
+# sudo apt-get update
+# sudo apt-get -y install cuda-drivers
 
 # install Rust CLI 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Add Rust and CUDA to PATH for all sessions
-echo 'export PATH=/usr/local/cuda/bin:$PATH' >> $HOME/.profile
+# echo 'export PATH=/usr/local/cuda/bin:$PATH' >> $HOME/.profile
 echo 'export PATH=$HOME/.cargo/bin:$PATH' >> $HOME/.profile
 source $HOME/.profile
 
 # Clone candle-core 
 git clone https://github.com/huggingface/candle.git
-cd candle
+# cd candle
